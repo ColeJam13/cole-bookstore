0b5f097fb45399912b187ca1a7ddc42d81469b32
ColeJam13 <colejam13@gmail.com>
2026-03-23 15:28:45 -0400

    Added methods to HomeController and Bookservice, as well as javadocs.

--- src/main/java/com/ksm/bookstore/controller/HomeController.java
+++ src/main/java/com/ksm/bookstore/controller/HomeController.java
@@ -5,9 +5,16 @@ import java.io.Serializable;
 import org.omnifaces.cdi.ViewScoped;
 
 import com.ksm.bookstore.service.BookService;
+import com.ksm.bookstore.jpa.Book;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.List;
+import java.util.Collections;
 
 /**
  * Controller for the public home page of the bookstore application.
@@ -17,11 +24,44 @@ import javax.inject.Named;
 
 @Named
 @ViewScoped
+@Getter
+@Setter
 public class HomeController implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Inject
     private BookService bookService;
+
+    private String searchTerm;
+
+    private String searchType;
+
+    private String sortField;
+
+    private List<Book> searchResults;
     
+
+    /**
+     * Method that runs automatically once the page is contructed to
+     * get All the books from the database and have them available to search 
+     */
+    @PostConstruct
+    public void init() {
+        searchResults = bookService.getAllBooks();
+    }
+
+    /**
+     * Method that runs when a user searches for a Book object(s). Depending on
+     * user input, will select the matching field to search by
+     */
+    public void search() {
+        if (searchType.equals("title")) {
+            searchResults = Collections.singletonList(bookService.getBookByTitle(searchTerm));
+        } else if (searchType.equals("author")) {
+            searchResults = bookService.getBookByAuthor(searchTerm);
+        } else if (searchType.equals("isbn")) {
+            searchResults = Collections.singletonList(bookService.getBookByIsbn(searchTerm));
+        }
+    }
 }
