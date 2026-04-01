1b91addcc8dd8c1f20cb1f0cd00b5842aad71436
ColeJam13 <colejam13@gmail.com>
2026-04-01 13:07:47 -0400

    Created CheckoutForm for gathering customer info

---
+++ src/main/java/com/ksm/bookstore/form/CheckoutForm.java
@@ -0,0 +1,43 @@
+package com.ksm.bookstore.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+import org.omnifaces.cdi.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+@Getter
+@Setter
+public class CheckoutForm implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String streetAddress;
+
+    private String city;
+
+    private String state;
+
+    private String zip;
+
+    private boolean sameAsShipping;
+
+    private String billingStreetAddress;
+
+    private String billingCity;
+
+    private String billingState;
+
+    private String billingZip;
+    
+}
