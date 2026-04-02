31a9e3f8b9cfc5b693c68afd42ee4db7aa10d4e6
ColeJam13 <colejam13@gmail.com>
2026-04-02 08:50:09 -0400

    Created ConfirmationForm

---
+++ src/main/java/com/ksm/bookstore/form/ConfirmationForm.java
@@ -0,0 +1,27 @@
+package com.ksm.bookstore.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import com.ksm.bookstore.jpa.Order;
+import com.ksm.bookstore.jpa.OrderItem;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.omnifaces.cdi.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+@Getter
+@Setter
+public class ConfirmationForm implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Order order;
+
+    private List<OrderItem> orderItems;
+    
+}
