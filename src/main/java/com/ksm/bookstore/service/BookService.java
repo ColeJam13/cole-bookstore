366273603517c16031357293172bf937403163c2
ColeJam13 <colejam13@gmail.com>
2026-04-03 16:55:37 -0400

    Updated bookservice to be stateless

--- src/main/java/com/ksm/bookstore/service/BookService.java
+++ src/main/java/com/ksm/bookstore/service/BookService.java
@@ -3,7 +3,7 @@ package com.ksm.bookstore.service;
 import java.util.Collections;
 import java.util.List;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import com.ksm.bookstore.dao.AuthorManager;
@@ -16,7 +16,7 @@ import com.ksm.bookstore.jpa.Book;
  * Acts as an intermediary between BookManager and AuthorManager and the controller layer.
  */
 
-@ApplicationScoped
+@Stateless
 public class BookService {
 
     @Inject
