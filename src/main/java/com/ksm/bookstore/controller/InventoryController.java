a96ab0c977ce25107f84700a2bc4e86c3c746063
ColeJam13 <colejam13@gmail.com>
2026-04-07 11:59:15 -0400

    Created the inventory controller with the methods an admin uses to edit the library

--- src/main/java/com/ksm/bookstore/controller/InventoryController.java
+++ src/main/java/com/ksm/bookstore/controller/InventoryController.java
@@ -1,31 +1,75 @@
 package com.ksm.bookstore.controller;
 
-import java.io.Serializable;
+import com.ksm.bookstore.dao.AuthorManager;
+import com.ksm.bookstore.dao.BookManager;
+import com.ksm.bookstore.form.InventoryForm;
+import com.ksm.bookstore.jpa.Author;
+import com.ksm.bookstore.jpa.Book;
 
-import org.omnifaces.cdi.ViewScoped;
-
-import com.ksm.bookstore.service.AuthorService;
-import com.ksm.bookstore.service.BookService;
-
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
  * Controller for the admin inventory management page of the bookstore application.
  * Handles adding, editing, and deleting books and authors for administrators.
- * Communicates with BookService and AuthorService to manage inventory data.
  */
 
 @Named
-@ViewScoped
-public class InventoryController implements Serializable {
-
-    private static final long serialVersionUID = 1L;
+@RequestScoped
+public class InventoryController {
 
     @Inject
-    private BookService bookService;
+    private BookManager bookManager;
 
     @Inject
-    private AuthorService authorService;
+    private AuthorManager authorManager;
+
+    @Inject
+    private InventoryForm inventoryForm;
+
+    /**
+     * Method that will either save a new Book object if the ID does not exist,
+     * or updates existing book if ID does exist
+     */
+    public void saveBook() {
+        if (inventoryForm.getSelectedBook().getBookId() == null) {
+            bookManager.create(inventoryForm.getSelectedBook());
+        } else {
+            bookManager.update(inventoryForm.getSelectedBook());
+        }
+        inventoryForm.setBookList(bookManager.findAll());
+    }
+
+    /**
+     * Method that will delete an existing book
+     * @param book
+     */
+    public void deleteBook(Book book) {
+        bookManager.delete(book);
+        inventoryForm.setBookList(bookManager.findAll());
+    }
+
+    /**
+     * Method that will either save a new Author object if the ID does not exist,
+     * or updates existing Author if ID does exist
+     */
+    public void saveAuthor() {
+        if (inventoryForm.getSelectedAuthor().getAuthorId() == null) {
+            authorManager.create(inventoryForm.getSelectedAuthor());
+        } else {
+            authorManager.update(inventoryForm.getSelectedAuthor());
+        }
+        inventoryForm.setAuthorList(authorManager.findAll());
+    }
+
+    /**
+     * Method that will delete an existing author
+     * @param author
+     */
+    public void deleteAuthor(Author author) {
+        authorManager.delete(author);
+        inventoryForm.setAuthorList(authorManager.findAll());
+    }
     
 }
