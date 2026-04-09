1d99d42de558108d0f42e660cca4d78f93538bdd
ColeJam13 <colejam13@gmail.com>
2026-04-09 11:04:41 -0400

    Changed checkout form to hold a customer object and search for an existing customer on init

--- src/main/java/com/ksm/bookstore/form/CheckoutForm.java
+++ src/main/java/com/ksm/bookstore/form/CheckoutForm.java
@@ -6,6 +6,14 @@ import lombok.Setter;
 import java.io.Serializable;
 
 import org.omnifaces.cdi.ViewScoped;
+
+import com.ksm.bookstore.dao.CustomerManager;
+import com.ksm.bookstore.jpa.Address;
+import com.ksm.bookstore.jpa.Customer;
+import com.ksm.bookstore.provider.UserProvider;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
@@ -19,28 +27,32 @@ public class CheckoutForm implements Serializable{
 
     private static final long serialVersionUID = 1L;
 
-    private String email;
-
-    private String firstName;
-
-    private String lastName;
-
-    private String streetAddress;
-
-    private String city;
-
-    private String state;
-
-    private String zip;
+    private Customer customer;
 
     private boolean sameAsShipping;
 
-    private String billingStreetAddress;
+    @Inject
+    private UserProvider userProvider;
 
-    private String billingCity;
+    @Inject
+    private CustomerManager customerManager;
 
-    private String billingState;
+    /**
+     * checks to see if there is an existing user for the entered username/email
+     * if so, finds that customer and populates the information in the form. If not
+     * found, creates new customer with new address fields
+     */
+    @PostConstruct
+    public void init() {
+        String username = userProvider.getUserName();
+        Customer existing = customerManager.findByEmail(username);
 
-    private String billingZip;
-    
+        if (existing != null) {
+            customer = existing;
+        } else {
+            customer = new Customer();
+            customer.setShippingAddress(new Address());
+            customer.setBillingAddress(new Address());
+        }
+    }
 }
